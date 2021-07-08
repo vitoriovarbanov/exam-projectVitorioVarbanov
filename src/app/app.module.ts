14abdb25716f3c11ca97685e669b6d4e070e2fd5
Vitorio Varbanov <vitorio.0405@hotmail.com>
2021-07-08 19:14:00 +0300

    added scss styles and config for app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,24 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularMaterialModule } from './angular-material/angular-material.module';
 import { HttpClientModule } from '@angular/common/http';
 
+import { SharedModule } from './shared/shared.module';
+import { HomeModule } from './home/home.module';
+
+// FIREBASE
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCSMVEI-JeMqnZkV0xINRC7aDOpR_zIvE4",
+  authDomain: "health-web-shop.firebaseapp.com",
+  projectId: "health-web-shop",
+  storageBucket: "health-web-shop.appspot.com",
+  messagingSenderId: "814139156034",
+  appId: "1:814139156034:web:5dee0632d0f82b7da4efdb"
+};
+
 
 @NgModule({
   declarations: [
@@ -18,6 +36,10 @@ import { HttpClientModule } from '@angular/common/http';
     AngularMaterialModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    SharedModule,
+    HomeModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule, // auth
   ],
   providers: [],
   bootstrap: [AppComponent],
