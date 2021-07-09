b395c558a817964f82407c24999cc5ae46719b03
Vitorio Varbanov <vitorio.0405@hotmail.com>
2021-07-09 10:57:27 +0300

    added more components for home page view

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,15 +1,23 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AngularMaterialModule } from '../angular-material/angular-material.module';
+
 
 import { HomeRoutingModule } from './home-routing.module';
 import { TopViewComponent } from './top-view/top-view.component';
+import { HomeView2Component } from './home-view2/home-view2.component';
 
 
 @NgModule({
-  declarations: [TopViewComponent],
+  declarations: [TopViewComponent, HomeView2Component],
   imports: [
     CommonModule,
-    HomeRoutingModule
-  ]
+    HomeRoutingModule,
+    AngularMaterialModule
+  ],
+  exports: [
+    TopViewComponent,
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class HomeModule { }
