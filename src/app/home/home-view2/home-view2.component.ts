06a4411f9bd0e6522cb91fd866e5b1460a511300
Vitorio Varbanov <vitorio.0405@hotmail.com>
2021-07-11 21:08:52 +0300

    added more images & 404 view & more markup

--- src/app/home/home-view2/home-view2.component.ts
+++ src/app/home/home-view2/home-view2.component.ts
@@ -6,6 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-view2.component.css']
 })
 export class HomeView2Component implements OnInit {
+  displayedViews = [
+    { src: '../../../assets/1.jpg', text: 'Lorem Ipsum Text Nice'},
+    { src: '../../../assets/2.jpg', text: 'Lorem Ipsum Text Nice'},
+    { src: '../../../assets/3.jpg', text: 'Lorem Ipsum Text Nice'},
+    { src: '../../../assets/health4.jpg', text: 'Lorem Ipsum Text Nice'},
+    { src: '../../../assets/health5.jpg', text: 'Lorem Ipsum Text Nice'},
+  ]
 
   constructor() { }
 
